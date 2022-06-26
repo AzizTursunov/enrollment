a953011acee3a28d2e6b3badd356ae3f8bdf5a9e
Aziz Tursunov <az.tursunov@gmail.com>
2022-06-26 22:07:41 +0300

    Отредактировал модель ShopUnit

--- products/shop_units/models.py
+++ products/shop_units/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class ShopUnit(models.Model):
+    """Создание модели ShopUnit"""
+
+    class ShopUnitType(models.TextChoices):
+        """Вспомогательный класс для выбора типа объекта."""
+
+        OFFER = 'OFFER', 'OFFER'
+        CATEGORY = 'CATEGORY', 'CATEGORY'
+
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    parentId = models.UUIDField(blank=True, default=None, null=True)
+    type = models.CharField(choices=ShopUnitType.choices, max_length=8)
+    price = models.PositiveIntegerField(blank=True, null=True)
