18dfd6b53057b91b6c6e2adb8db062b8177fdbbc
Aziz Tursunov <az.tursunov@gmail.com>
2022-06-26 22:12:42 +0300

    Связал урлы проекта с урлами API

--- products/api/urls.py
+++ products/api/urls.py
@@ -4,9 +4,9 @@ from rest_framework.routers import DefaultRouter
 from .views import ShopUnitImport, ShopUnitDestroy, ShopUnitDetail, ShopUnitSales
 
 urlpatterns = [
-    path('v1/imports/', ShopUnitImport.as_view()),
-    path('v1/delete/<str:pk>', ShopUnitDestroy.as_view()),
-    path('v1/nodes/<str:pk>', ShopUnitDetail.as_view()),
-    path('v1/sales/', ShopUnitSales.as_view()),
+    path('imports/', ShopUnitImport.as_view()),
+    path('delete/<str:pk>', ShopUnitDestroy.as_view()),
+    path('nodes/<str:pk>', ShopUnitDetail.as_view()),
+    path('sales/', ShopUnitSales.as_view()),
 
 ]
